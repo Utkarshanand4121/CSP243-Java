5d2316da5463f6cb4f92bb8ea1b5adc0b9238797
Utkarsh Anand <utkarshanand4121@gmail.com>
2023-12-07 10:57:22 +0530

    Exam Practice

--- Value Added/CollegePractice.java
+++ Value Added/CollegePractice.java
@@ -66,7 +66,89 @@ class Childr extends Parents{
     }
 }
 
+// Question no = 10(a)
+interface A1 {
+    int constantA = 50;
+}
+interface A2 {
+    int constantA2 = 100;
+}
+interface A12 extends A1,A2 {
+    int constant = 75;
+}
+class B implements A12 {
+    int constant2 = 70;
 
+    void method1() {
+        System.out.println("Method1 is called");
+    }
+
+    void method2() {
+        System.out.println("Method2 is called");
+    }
+}
+
+// Question no = 10(b)
+interface Vehicle{
+    void stop();
+    void start();
+}
+interface Luxury{
+    void parking();
+}
+class Car implements Vehicle,Luxury {
+    @Override
+    public void stop() {
+        System.out.println("Stop the car when traffic signal is red");
+    }
+    @Override
+    public void start() {
+        System.out.println("Start the car when traafic signal is green");
+    }
+    @Override
+    public void parking() {
+        System.out.println("Park the car in right place");
+    }
+}
+
+// Question no = 9(a)
+class Integer {
+    int num;
+    char c;
+
+    void name(int num, char c) {
+        System.out.println("Number is = "+num+" Character is = "+c);
+    }
+
+    void name(char c, int num) {
+        System.out.println("Character is = "+c+" Number is = "+num);
+    }
+}
+
+// Question no = 11(b)
+class Example{
+    static int count = 0;
+
+    static void display() {
+        System.out.println("Static method");
+    }
+
+    static{
+        System.out.println("Static block");
+    }
+}
+
+// Question no = 12(a)
+class Even{
+    void evenNum() {
+        for(int i=1; i<=100; i++) {
+            if(i%2==0) {
+                System.out.print("Even Number = "+i);
+            }
+            System.out.println();
+        }
+    }
+}
 public class CollegePractice {
     public static void main(String[] args) {
         // Number n1 = new Number();
@@ -95,11 +177,30 @@ public class CollegePractice {
         // System.out.println("str2 index number 2 having character is : "+str2.charAt(1));
         // System.out.println(str1.compareTo(str2));
 
-        Parents p1 = new Parents();
-        p1.setDetails("Utkarsh", 2);
-        p1.getDetails();
-        Childr c1 = new Childr();
-        c1.details(2);
-        c1.get();
+        // Parents p1 = new Parents();
+        // p1.setDetails("Utkarsh", 2);
+        // p1.getDetails();
+        // Childr c1 = new Childr();
+        // c1.details(2);
+        // c1.get();
+
+        // B b1 = new B();
+        // b1.method1();
+        // b1.method2();
+
+        // Car c1 = new Car();
+        // c1.start();
+        // c1.stop();
+        // c1.parking();
+
+        // Integer i = new Integer();
+        // i.name(5, 'U');
+        // i.name('A', 10);
+
+        // Example e1 = new Example();
+        // Example.display();
+        // System.out.println("Count = "+Example.count);
+        Even e1 = new Even();
+        e1.evenNum();
     }
 }
